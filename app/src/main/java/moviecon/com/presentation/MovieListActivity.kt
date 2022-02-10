06d8120bb2a7883facbdf8c056c9df25f6a83c87
SergeevSezar <sergeev.sezar@yandex.ru>
2022-02-11 06:37:04 +0900

    upd MovieListActivity

--- app/src/main/java/moviecon/com/presentation/MovieListActivity.kt
+++ app/src/main/java/moviecon/com/presentation/MovieListActivity.kt
@@ -2,17 +2,29 @@ package moviecon.com.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import moviecon.com.R
+import moviecon.com.databinding.ActivityMovieListBinding
+import moviecon.com.presentation.adapter.MovieAdapter
 
 class MovieListActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MovieViewModel
 
+    private val binding by lazy {
+        ActivityMovieListBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_list)
-        viewModel = ViewModelProvider(this)[MovieViewModel::class.java]
-        viewModel.loadData()
+        setContentView(binding.root)
+
+        val adapter = MovieAdapter(this)
+        binding.rvMoviePoster.adapter = adapter
+
+        viewModel = ViewModelProvider.AndroidViewModelFactory(application).create(MovieViewModel::class.java)
+        viewModel.movieList.observe(this, Observer {
+            adapter.movieList = it
+        })
     }
 }

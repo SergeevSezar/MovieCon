969feb272c345ab207806d1cd2dd4ddd0a2be77d
SergeevSezar <sergeev.sezar@yandex.ru>
2022-03-08 06:55:40 +0900

    added correct ful url for load poster

--- app/src/main/java/moviecon/com/data/network/ApiFactory.kt
+++ app/src/main/java/moviecon/com/data/network/ApiFactory.kt
@@ -6,6 +6,17 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiFactory {
     private const val BASE_URL = "https://api.themoviedb.org/3/"
+    private const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/"
+    private const val BIG_POSTER_SIZE = "w780"
+    private const val SMALL_POSTER_SIZE = "w180"
+
+    fun getFullBigImageUrl(posterPath: String):String {
+        return POSTER_BASE_URL + BIG_POSTER_SIZE + posterPath;
+    }
+
+    fun getFullSmallImageUrl(posterPath: String):String {
+        return POSTER_BASE_URL + SMALL_POSTER_SIZE + posterPath;
+    }
 
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
